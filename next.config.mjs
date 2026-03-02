b42cf05138cc00328a03971446052f6e4b338721
KeisandyDafaMulianda <keisandydafamulianda@gmail.com>
2026-03-03 01:03:17 +0700

    Fix next config build error

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,13 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: true,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default withPWA(nextConfig);
